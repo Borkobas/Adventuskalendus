89dbffe6923d736eeb829adb144a7de433f66eaf
Borkobas <boris.nemet@gmail.com>
2022-11-28 00:21:37 +0100

    von Anfang an beginnen
    Ich habe versucht, meinen vorherigen Code zu ändern, um ein Feld (Box) öffnen zu können, und dann gibt es in diesem Feld verschiedene interessante Grafiken, die mit p5.js erzeugt werden. Letzte Woche hatte Domestika einen großen Blackfriday-Sale, also kaufte ich zwei Kurse über kreative JavaScript-Codierung:
    https://www.domestika.org/de/bruno_imbrizi
    
    Es ist nicht etwas, von dem ich denke, dass es leicht möglich ist, es professionell zu verfolgen, aber es ist wirklich interessant und es ist Javascript, also denke ich, dass es mir helfen kann, js schneller zu verstehen / zu lernen.
    Wie auch immer, der letzte Code war etwas kompliziert und Sie sagten, wir sollten versuchen, es so einfach wie möglich zu machen. Also habe ich versucht, es so einfach wie möglich zu machen. Heute werde ich den letzten großen Schritt hinzufügen, Grafiken zum Mix. Und dann Polieren. Ich habe ein kleines Problem, nur die erste Taste funktioniert richtig.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-$(".dietuer").on("click", function(e){
-  e.target.classList.toggle("dietueroffen");
-})
+var btn = document.getElementById('knopf');
+btn.addEventListener("click", function(){
+  document.getElementById('feld').classList.toggle("offen-feld");
+});
+
+function toggleCLass() {
+   var myfeldClasses = document.getElementsByClassName('feld').classList;
+   if (myfeldClasses.contains("feld-scale")) {
+       myfeldClasses.remove("feld-scale");
+   } else {
+       myfeldClasses.add("feld-scale");
+   }
+}
